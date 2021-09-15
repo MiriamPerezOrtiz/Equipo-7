8ac43de863c1cf3e4a352958db505cb2dad5445c
Paulina Alva <a01750624@itesm.mx>
2021-09-14 22:07:31 -0500

    Version Pau

--- Actividad 2 - Snake.py
+++ Actividad 2 - Snake.py
@@ -6,11 +6,14 @@ food = vector(0, 0)
 snake = [vector(10, 0)]
 aim = vector(0, -10)
 
-#Elegir la velocidad de Snake
+# Elegir la velocidad de Snake
 v = 0
+
+
 def inicio(velocidad):
     print("¡Bienvenido al Snake Game!")
-    velocidad = input("Elija la velocidad de Snake: Alto, Medio, Bajo.\nVelocidad: ")
+    velocidad = input(
+        "Elija la velocidad de Snake: Alto, Medio, Bajo.\nVelocidad: ")
     if velocidad.lower() == "alto":
         return 20
     elif velocidad.lower() == "medio":
@@ -20,26 +23,49 @@ def inicio(velocidad):
     else:
         print("Elija una de las 3 opciones")
         inicio()
-#Elegir la velocidad de Snake
+# Elegir la velocidad de Snake
+
 
 def change(x, y):
     "Change snake direction."
     aim.x = x
     aim.y = y
 
+
 def inside(head):
     "Return True if head inside boundaries."
     return -200 < head.x < 190 and -200 < head.y < 190
+
 
 def move():
     "Move snake forward one segment."
     head = snake[-1].copy()
     head.move(aim)
 
-    if not inside(head) or head in snake:
+    if head in snake:
         square(head.x, head.y, 9, 'red')
         update()
         return
+
+    if head.x == 190:
+        change(-v, 0)
+        head = snake[-1].copy()
+        head.move(aim)
+
+    if head.x == -200:
+        change(v, 0)
+        head = snake[-1].copy()
+        head.move(aim)
+
+    if head.y == -200:
+        change(0, v)
+        head = snake[-1].copy()
+        head.move(aim)
+
+    if head.y == 190:
+        change(0, -v)
+        head = snake[-1].copy()
+        head.move(aim)
 
     snake.append(head)
 
@@ -58,6 +84,7 @@ def move():
     square(food.x, food.y, 9, 'green')
     update()
     ontimer(move, 100)
+
 
 setup(420, 420, 370, 0)
 hideturtle()
